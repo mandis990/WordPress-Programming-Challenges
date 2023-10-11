5e5f8ed20daf3498540ff2fa5c5183beb85e2135
Manish <manish.chopra@xecurify.com>
2023-10-12 01:05:30 +0530

    Added new button on WordPress admin menu bar

--- slideshow-plugin/rtcamp_slideshow_main.php
+++ slideshow-plugin/rtcamp_slideshow_main.php
@@ -1,11 +1,32 @@
 <?php
 /**
  * Plugin Name: Slideshow Plugin
- * Plugin URI: https://rtcamp.com
- * Description: Allows to display slideshow
+ * Plugin URI: https://rtcamp.com/
+ * Description: Allows to upload photos and create beautiful slideshow.
  * Version: 1.0.0
- * Author: 
- * Author URI: https://rtcamp.com
- * License: rTCamp
+ * Author: RT Camp
+ * Author URI: https://rtcamp.com/
+ * Text Domain: rtcamp-slideshow-plugin
+ * License: miniOrange
  */
-?>
+
+// Add a plugin button to the admin bar.
+add_action( 'admin_menu', 'my_menu' );
+
+/**
+ * Creating new admin menu
+ *
+ * @return void
+ */
+function my_menu() {
+	add_menu_page( 'My Page Title', 'Image Slider Pro', 'manage_options', 'my-page-slug', 'my_function' );
+}
+
+/**
+ * Main content of the page
+ *
+ * @return void
+ */
+function my_function() {
+	echo 'This is the rT Camp Slideshow Plugin!';
+}
