1ddf14ba7bf83420e783a0273776098097c7d993
Manish <manish.chopra@xecurify.com>
2023-10-11 20:55:36 +0530

    Added new folder for Slideshow plugin

---
+++ slideshow-plugin/rtcamp_slideshow_main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Slideshow Plugin
+ * Plugin URI: https://rtcamp.com
+ * Description: Allows to display slideshow
+ * Version: 1.0.0
+ * Author: 
+ * Author URI: https://rtcamp.com
+ * License: rTCamp
+ */
+?>
